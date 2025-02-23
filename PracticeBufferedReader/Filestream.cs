8f520a1f89cc84d8e9ccd934c2589d988ff35048
Shubham Das <subhamdas9800@gmail.com>
2025-02-23 22:34:23 +0530

    StreamReader Binary linear Search

---
+++ PracticeBufferedReader/Filestream.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+class FileStreamExample
+{
+    static void Main()
+    {
+        try
+        {
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\sample.txt");
+            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                int i;
+                while ((i = fs.ReadByte()) != -1)
+                {
+                    Console.Write((char)i);
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+        }
+    }
+}
---
+++ PracticeBufferedReader/Program.cs
@@ -0,0 +1,37 @@
+﻿//using System;
+//using System.IO;
+//using System.Net.Sockets;
+
+//class Program
+//{
+//    public static void Main(string[] args)
+//    {
+//        //string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\sample.txt");
+//        //// File must exist in the project directory
+
+//        //try
+//        //{
+//        //    using (StreamReader sr = new StreamReader(filePath))
+//        //    {
+//        //        string line;
+//        //        while ((line = sr.ReadLine()) != null)
+//        //        { // Reads line by line
+//        //            Console.WriteLine(line);
+//        //        }
+//        //    }
+//        //}
+//        //catch (IOException e)
+//        //{
+//        //    Console.WriteLine("Error: " + e.Message);
+//        //}
+
+
+//        //// READING FROM A NETWORK STREAM
+//        //using (TcpClient client = new TcpClient("example.com", 80))
+//        //using (NetworkStream ns = client.GetStream())
+//        //using (StreamReader sr = new StreamReader(ns))
+//        //{
+//        //    Console.WriteLine(sr.ReadToEnd()); // Read data from the server
+//        //}
+//    }
+//}
